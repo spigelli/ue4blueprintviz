4412e0a36d367904de365fe1f2d411ce577548de
spigelli <isaacspiegel@gmail.com>
2021-02-22 04:41:47 -0600

    Fixed dependencies for gulp4, gulp-tsc and node 14

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
-
+var typescript = require('typescript')
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var typescript = require('gulp-tsc');
-var Server = require('karma').Server;
+var ts = require('gulp-typescript');
+
+//var Server = require('karma').Server;
 
 var dest = 'dest/';
 
@@ -24,30 +25,42 @@ gulp.task('watch', function() {
     gulp.watch('index.html', ['copy']);
 });
 
-gulp.task('compile', function(){
-    var options = {
-        target: 'ES5',
-        out: 'ue4lib.js'
-    };
+// gulp.task('compile', function(){
+//     var options = {
+//         target: 'ES5',
+//         out: 'ue4lib.js'
+//     };
 
-    gulp.src(['src/Main.ts'])
-        .pipe(typescript(options))
+//     gulp.src(['src/Main.ts'])
+//         .pipe(ts(options))
+//         .pipe(gulp.dest(dest));
+// });
+
+gulp.task('compile', ()=>{
+    return gulp.src('src/Main.ts')
+        .pipe(ts({
+            target: 'ES5',
+            out: 'ue4lib.js'
+        }))
         .pipe(gulp.dest(dest));
-});
+})
 
 gulp.task('copy', function(){
     gulp.src('index.html')
         .pipe(gulp.dest(dest));
 });
 
-gulp.task('test', function (done) {
-    new Server({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done).start();
-});
+//gulp.task('test', function (done) {
+//    new Server({
+//        configFile: __dirname + '/karma.conf.js',
+//        singleRun: true
+//    }, done).start();
+//});
 
-gulp.task('default', ['scripts', 'watch']);
-gulp.task('production', ['compile', 'copy']);
-gulp.task('minify', ['minifyjs']);
+//gulp.task('default', function(){
+//    ['scripts', 'watch']
+//    });
+gulp.task('default', gulp.parallel('compile', 'watch'));
+//gulp.task('production', ['compile', 'copy']);
+//gulp.task('minify', ['minifyjs']);
 //todo gulp.task('release',...)
